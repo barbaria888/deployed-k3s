0d8daf41571e3598380e1d1c92e2f800e2b441bb
HARDIK ARORA <arorahardik0811@gmail.com>
2026-02-10 15:35:32 +0530

    Update running-prod.sh to run prerequisites script
    Fix indentation and add execution permission for prerequisites script.

--- running-prod.sh
+++ running-prod.sh
@@ -1,2 +1,4 @@
 git clone -b production https://github.com/Dhruvsahu1/Educonnect-D.git
- cd Educonnect-D/infrastructure
+cd Educonnect-D/infrastructure
+chmod +x prerequesites.sh
+./prerequesites.sh
